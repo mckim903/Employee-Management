7becfad096c7bf0bc46935a8a9289d697ecb788a
huidadao <huidadao@gmail.com>
2026-07-01 19:57:53 +0900

    feat: add authentication, layout, and page components

---
+++ client/src/pages/Employees.tsx
@@ -0,0 +1,11 @@
+
+
+const Employees = () => {
+  return (
+    <div>
+      <h1>Employees</h1>
+    </div>
+  )
+}
+
+export default Employees
---
+++ client/src/pages/PrintPayslip.tsx
@@ -0,0 +1,11 @@
+
+
+const PrintPayslip = () => {
+  return (
+    <div>
+      <h1>Print Payslip</h1>
+    </div>
+  )
+}
+
+export default PrintPayslip
