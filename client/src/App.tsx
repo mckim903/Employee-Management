bd5d91ee890863aa53267abf086cca09e905961f
huidadao <huidadao@gmail.com>
2026-07-04 10:30:43 +0900

    frontend completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ const App = () => {
           <Route path='/settings' element={<Settings />} />
         </Route>
         
-        <Route path='/print/payslip/:id' element={<PrintPayslip />} />
+        <Route path='/print/payslips/:id' element={<PrintPayslip />} />
         <Route path='*' element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </>
