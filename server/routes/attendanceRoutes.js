e31a756f62f4b532ce82ab70f5bdf962bdaf479f
huidadao <huidadao@gmail.com>
2026-07-05 08:42:22 +0900

    API created for leave Application

--- server/routes/attendanceRoutes.js
+++ server/routes/attendanceRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express"
 import { protect } from "../middleware/auth.js"
-import { clockInOut, getAttendance } from "../controllers/attendanceController"
+import { clockInOut, getAttendance } from "../controllers/attendanceController.js"
 
 
 const attendanceRouter = Router()
 
-attendanceRouter,post("/", protect, clockInOut)
-attendanceRouter,get("/", protect, getAttendance)
+attendanceRouter.post("/", protect, clockInOut)
+attendanceRouter.get("/", protect, getAttendance)
 
 export default attendanceRouter;
